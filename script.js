7d204f4a5879e2067332ee11fd4e0c2ac3a28e04
Fedor <qwertyfredya@gmail.com>
2020-06-29 22:40:43 +0700

    second variant

--- script.js
+++ script.js
@@ -1,25 +1,8 @@
-const hours = document.querySelector('.time__hours')
-const minutes = document.querySelector('.time__minutes')
-const seconds = document.querySelector('.time__seconds')
-
-
-function getSeconds() {
-    const date = new Date()
-    return date.getSeconds()
-}
-function getMinutes() {
-    const date = new Date()
-    return date.getMinutes()
-}
-function getHours() {
-    const date = new Date()
-    return date.getHours()
-}
-
 function changeTime() {
-    seconds.textContent = getSeconds()
-    minutes.textContent = getMinutes()
-    hours.textContent = getHours()
+  const date = new Date()
+  document.querySelector('.time__hours').textContent = date.getHours();
+  document.querySelector('.time__minutes').textContent = date.getMinutes();
+  document.querySelector('.time__seconds').textContent = date.getSeconds();
 }
 
 setInterval(changeTime, 1000)
