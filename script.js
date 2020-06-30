595357218e5a7481956475bf32f83ca01a89b193
Fedor <qwertyfredya@gmail.com>
2020-06-30 09:03:26 +0700

    fourth version

--- script.js
+++ script.js
@@ -2,23 +2,38 @@ function changeTime() {
   const date = new Date()
   checkHours(date.getHours())
   checkMinutes(date.getMinutes())
-  document.querySelector('.time__seconds').textContent = date.getSeconds();
+  checkSeconds(date.getSeconds())
 }
 
 function checkHours(hour) {
-  const hours = document.querySelector('.time__hours').textContent
-  if (hour !== Number(hours)) {
-    document.querySelector('.time__hours').textContent = hour
+  const hourElement = document.querySelector('.time__hours').textContent
+  let newHour = hour
+  if (newHour !== Number(hourElement)) {
+    document.querySelector('.time__hours').textContent = checkWriting(newHour)
     console.log('перерисовка часов произошла')
   }
 }
 
 function checkMinutes(minute) {
-  const minutes = document.querySelector('.time__minutes').textContent
-  if (minute !== Number(minutes)) {
-    document.querySelector('.time__minutes').textContent = minute
+  const minuteElement = document.querySelector('.time__minutes').textContent
+  let newMinunte = minute
+  if (newMinunte !== Number(minuteElement)) {
+    document.querySelector('.time__minutes').textContent = checkWriting(newMinunte)
     console.log('перерисовка минуты произошла')
   }
+}
+
+function checkSeconds(second) {
+  document.querySelector('.time__seconds').textContent = checkWriting(second)
+}
+
+
+function checkWriting(number) {
+  let newNumber = number
+  if (newNumber < 10) {
+    newNumber = `0${number}`
+  }
+  return newNumber
 }
 
 
