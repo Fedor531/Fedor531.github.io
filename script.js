803bee5ab6adf540de7620e88c5c601631307252
Fedor <qwertyfredya@gmail.com>
2020-06-30 09:49:23 +0700

    refactoring

--- script.js
+++ script.js
@@ -2,25 +2,21 @@ function changeTime() {
   const date = new Date()
   checkHours(date.getHours())
   checkMinutes(date.getMinutes())
-  checkSeconds(date.getSeconds())
+  document.querySelector('.time__seconds').textContent = checkWriting(date.getSeconds())
 }
 
 function checkHours(hour) {
   const hourElementText = document.querySelector('.time__hours').textContent
-  if (hour !== Number(hourElementText)) {
+  if (!(hour == hourElementText)) {
     document.querySelector('.time__hours').textContent = checkWriting(hour)
   }
 }
 
 function checkMinutes(minute) {
   const minuteElementText = document.querySelector('.time__minutes').textContent
-  if (minute !== Number(minuteElementText)) {
+  if (!(minute == minuteElementText)) {
     document.querySelector('.time__minutes').textContent = checkWriting(minute)
   }
-}
-
-function checkSeconds(second) {
-  document.querySelector('.time__seconds').textContent = checkWriting(second)
 }
 
 function checkWriting(number) {
