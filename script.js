fe0bb395ee2ba901fd79097a14f97da87e0f8825
Fedor <qwertyfredya@gmail.com>
2020-06-30 08:36:33 +0700

    third version

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 function changeTime() {
   const date = new Date()
-  document.querySelector('.time__hours').textContent = date.getHours();
-  document.querySelector('.time__minutes').textContent = date.getMinutes();
+  checkHours(date.getHours())
+  checkMinutes(date.getMinutes())
   document.querySelector('.time__seconds').textContent = date.getSeconds();
 }
+
+function checkHours(hour) {
+  const hours = document.querySelector('.time__hours').textContent
+  if (hour !== Number(hours)) {
+    document.querySelector('.time__hours').textContent = hour
+    console.log('перерисовка часов произошла')
+  }
+}
+
+function checkMinutes(minute) {
+  const minutes = document.querySelector('.time__minutes').textContent
+  if (minute !== Number(minutes)) {
+    document.querySelector('.time__minutes').textContent = minute
+    console.log('перерисовка минуты произошла')
+  }
+}
+
+
 
 setInterval(changeTime, 1000)
