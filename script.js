342687711f2b503ba73e3a209bcb3f4656073ee9
Fedor <qwertyfredya@gmail.com>
2020-07-01 21:52:29 +0700

    refactoring

--- script.js
+++ script.js
@@ -1,30 +1,44 @@
-function changeTime() {
-  const date = new Date()
-  checkHours(date.getHours())
-  checkMinutes(date.getMinutes())
-  document.querySelector('.time__seconds').textContent = checkWriting(date.getSeconds())
-}
+(function () {
 
-function checkHours(hour) {
-  const hourElementText = document.querySelector('.time__hours').textContent
-  if (!(hour == hourElementText)) {
-    document.querySelector('.time__hours').textContent = checkWriting(hour)
+  function changeTime() {
+    const date = new Date()
+    checkHours(date.getHours())
+    checkMinutes(date.getMinutes())
+    document.querySelector('.time__seconds').textContent = formatingNumber(date.getSeconds())
   }
-}
 
-function checkMinutes(minute) {
-  const minuteElementText = document.querySelector('.time__minutes').textContent
-  if (!(minute == minuteElementText)) {
-    document.querySelector('.time__minutes').textContent = checkWriting(minute)
+  function checkHours(hour) {
+    const hourElementText = document.querySelector('.time__hours').textContent
+    if (hour !== hourElementText) {
+      redrawHours(hour)
+    }
   }
-}
 
-function checkWriting(number) {
-  let newNumber = number
-  if (newNumber < 10) {
-    newNumber = `0${number}`
+  function redrawHours(hour) {
+    document.querySelector('.time__hours').textContent = formatingNumber(hour)
   }
-  return newNumber
-}
 
-setInterval(changeTime, 1000)
+  function checkMinutes(minute) {
+    const minuteElementText = document.querySelector('.time__minutes').textContent
+    if (minute !== minuteElementText) {
+      redrawMinutes(minute)
+    }
+  }
+
+  function redrawMinutes(minute) {
+    document.querySelector('.time__minutes').textContent = formatingNumber(minute)
+    console.log('перерисовка минуты произошла')
+  }
+
+
+  function formatingNumber(number) {
+    let newNumber = number
+    if (newNumber < 10) {
+      newNumber = `0${number}`
+    }
+    return newNumber
+  }
+
+  setInterval(changeTime, 1000)
+
+})()
