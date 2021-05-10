2a72fbb55d13270f95c600776a8426ba96024fe2
Fedor <qwertyfredya@gmail.com>
2021-05-10 16:29:09 +0300

    refactor: check function

--- script.js
+++ script.js
@@ -1,29 +1,21 @@
 (function () {
 
-    const hourNode = document.querySelector('.clock__hours')
+    const hourNode   = document.querySelector('.clock__hours')
     const minuteNode = document.querySelector('.clock__minutes')
     const secondNode = document.querySelector('.clock__seconds')
 
     function changeTime() {
         const date = new Date()
-        checkHours(date.getHours())
-        checkMinutes(date.getMinutes())
+        checkToRedraw(hourNode, date.getHours())
+        checkToRedraw(minuteNode, date.getMinutes())
         redrawNode(secondNode, date.getSeconds())
     }
 
     // Function to check redrawing
-    function checkHours(hour) {
-        const hourElementText = hourNode.textContent
-        if (hour !== hourElementText) {
-            redrawNode(hourNode, hour)
-        }
-    }
-
-    // Function to check redrawing
-    function checkMinutes(minute) {
-        const minuteElementText = minuteNode.textContent
-        if (minute !== minuteElementText) {
-            redrawNode(minuteNode, minute)
+    function checkToRedraw(node, value) {
+        const nodeElementText = +node.textContent
+        if (value !== nodeElementText) {
+            redrawNode(node, value)
         }
     }
 
